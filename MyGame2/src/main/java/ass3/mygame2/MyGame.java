8d1d010814b0a2913a5a34ce0809e23828eecf2e
mdrahman16 <80794888+mdrahman16@users.noreply.github.com>
2021-05-25 10:05:50 +1000

    Initial project without any changes
    It is project without made any changes

---
+++ MyGame2/src/main/java/ass3/mygame2/MyGame.java
@@ -0,0 +1,28 @@
+
+package ass3.mygame2;
+
+
+/**
+ *
+ * @author @version
+ */
+
+
+public class MyGame {
+
+    /**
+    *
+     * @param args
+    * @param nothing
+    */
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.play(); 
+        
+    }
+    
+    
+    
+}
+
+
