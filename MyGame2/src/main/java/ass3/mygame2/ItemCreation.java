1198d7accd8b0e65a5b6600a4a4505f2a3bb4c4a
mdrahman16 <80794888+mdrahman16@users.noreply.github.com>
2021-05-25 12:03:37 +1000

    version 1.1
    the items are added to the list

--- MyGame2/src/main/java/ass3/mygame2/ItemCreation.java
+++ MyGame2/src/main/java/ass3/mygame2/ItemCreation.java
@@ -22,16 +22,18 @@ public class ItemCreation
     
     public void createItems(){
         
-        Item item1, item2, excaliburSword, key, frontGateKey;
+        Item item1, item2, excaliburSword, key, frontGateKey,magicalKnife;
         
         
         excaliburSword = new Item("excaliburSword", "The legendary Excalibur", 100);
         key = new Item("key", "It has a shape of a heart", 100);
         frontGateKey = new Item("frontGateKey", "To open the front gate door", 100);
+        magicalKnife = new Item("Magical knife", "Knife with magical powers", 100);
         
         allItemsInGame.add(excaliburSword);
         allItemsInGame.add(key);
         allItemsInGame.add(frontGateKey);
+        allItemsInGame.add(magicalKnife);
     }
    
     public Item getItem(String stringItem){
